2951214d94447e8327b95b6c1c0657507d937926
kkllPreciel <zie35-gymiko@jcom.zaq.ne.jp>
2017-08-20 23:06:32 +0900

    タブをスペースへ修正

--- Sein/Sein/Direct3D12/buffer.cc
+++ Sein/Sein/Direct3D12/buffer.cc
@@ -1,9 +1,9 @@
 /**
- *	@file		buffer.cc
- *	@brief		バッファに関する処理を行うプログラムソース
- *	@author		kkllPreciel
- *	@date		2017/07/19
- *	@version	1.0
+ *  @file     buffer.cc
+ *  @brief    バッファに関する処理を行うプログラムソース
+ *  @author   kkllPreciel
+ *  @date     2017/07/19
+ *  @version  1.0
  */
 
  // include
@@ -14,7 +14,7 @@ namespace Sein
   namespace Direct3D12
   {
     /**
-     *	@brief	コンストラクタ
+     *  @brief  コンストラクタ
      */
     Buffer::Buffer() : buffer(nullptr)
     {
@@ -22,7 +22,7 @@ namespace Sein
     }
 
     /**
-     *	@brief	デストラクタ
+     *  @brief  デストラクタ
      */
     Buffer::~Buffer()
     {
@@ -30,11 +30,11 @@ namespace Sein
     }
 
     /**
-     *	@brief	リソース(バッファ)を生成する
-     *	@param	device:Direct3D12のデバイス
-     *	@param	properties:リソースのヒーププロパティ
-     *	@param	width:リソースのサイズ
-     *	@param	flag:リソース操作オプションフラグ
+     *  @brief  リソース(バッファ)を生成する
+     *  @param  device:Direct3D12のデバイス
+     *  @param  properties:リソースのヒーププロパティ
+     *  @param  width:リソースのサイズ
+     *  @param  flag:リソース操作オプションフラグ
      */
     void Buffer::Create(ID3D12Device* const device, const D3D12_HEAP_PROPERTIES& properties, const UINT64 width, const D3D12_RESOURCE_FLAGS flag) noexcept(false)
     {
@@ -42,26 +42,26 @@ namespace Sein
 
       // リソースの設定
       D3D12_RESOURCE_DESC resource_desc;
-      resource_desc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;		// リソースの種別(今回は必ずバッファ)
-      resource_desc.Alignment = 0;									// アラインメント
-      resource_desc.Width = width;									// リソースの幅
-      resource_desc.Height = 1;										// リソースの高さ(今回は必要数分の幅を確保しているので必ず1)
-      resource_desc.DepthOrArraySize = 1;								// リソースの深さ(テクスチャ等に使用する物、今回は必ず1)
-      resource_desc.MipLevels = 1;									// ミップマップのレベル(今回は必ず1)
-      resource_desc.Format = DXGI_FORMAT_UNKNOWN;						// リソースデータフォーマット(R8G8B8A8等)(今回は必ず不明)
-      resource_desc.SampleDesc.Count = 1;								// ピクセル単位のマルチサンプリング数
-      resource_desc.SampleDesc.Quality = 0;							// マルチサンプリングの品質レベル
-      resource_desc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;			// テクスチャレイアウトオプション
-      resource_desc.Flags = flag;										// リソース操作オプションフラグ
+      resource_desc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;  // リソースの種別(今回は必ずバッファ)
+      resource_desc.Alignment = 0;                                // アラインメント
+      resource_desc.Width = width;                                // リソースの幅
+      resource_desc.Height = 1;                                   // リソースの高さ(今回は必要数分の幅を確保しているので必ず1)
+      resource_desc.DepthOrArraySize = 1;                         // リソースの深さ(テクスチャ等に使用する物、今回は必ず1)
+      resource_desc.MipLevels = 1;                                // ミップマップのレベル(今回は必ず1)
+      resource_desc.Format = DXGI_FORMAT_UNKNOWN;                 // リソースデータフォーマット(R8G8B8A8等)(今回は必ず不明)
+      resource_desc.SampleDesc.Count = 1;                         // ピクセル単位のマルチサンプリング数
+      resource_desc.SampleDesc.Quality = 0;                       // マルチサンプリングの品質レベル
+      resource_desc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;      // テクスチャレイアウトオプション
+      resource_desc.Flags = flag;                                 // リソース操作オプションフラグ
 
       // リソース(バッファ)の生成
       ID3D12Resource* resource = nullptr;
       if (FAILED(device->CreateCommittedResource(
-        &properties,						// ヒープの設定
-        D3D12_HEAP_FLAG_NONE,				// ヒープオプション(TODO:適切なオプションを設定する)
-        &resource_desc,						// リソースの設定
-        D3D12_RESOURCE_STATE_GENERIC_READ,	// リソースの状態(TODO:適切な状態を設定する)
-        nullptr,							// クリアカラーのデフォルト値
+        &properties,                        // ヒープの設定
+        D3D12_HEAP_FLAG_NONE,               // ヒープオプション(TODO:適切なオプションを設定する)
+        &resource_desc,                     // リソースの設定
+        D3D12_RESOURCE_STATE_GENERIC_READ,  // リソースの状態(TODO:適切な状態を設定する)
+        nullptr,                            // クリアカラーのデフォルト値
         IID_PPV_ARGS(&resource))))
       {
         throw "リソース(バッファ)の作成に失敗しました。";
@@ -70,7 +70,7 @@ namespace Sein
     }
 
     /**
-     *	@brief	リソース(バッファ)を開放する
+     *  @brief  リソース(バッファ)を開放する
      */
     void Buffer::Release() noexcept
     {
@@ -81,8 +81,8 @@ namespace Sein
     }
 
     /**
-     *	@brief	リソース(バッファ)を取得する
-     *	@return	リソースの参照
+     *  @brief  リソース(バッファ)を取得する
+     *  @return リソースの参照
      */
     ID3D12Resource& Buffer::Get() const noexcept
     {
--- Sein/Sein/Direct3D12/buffer.h
+++ Sein/Sein/Direct3D12/buffer.h
@@ -1,9 +1,9 @@
 /**
- *	@file		buffer.h
- *	@brief		バッファに関するヘッダファイル
- *	@author		kkllPreciel
- *	@date		2017/07/19
- *	@version	1.0
+ *  @file     buffer.h
+ *  @brief    バッファに関するヘッダファイル
+ *  @author   kkllPreciel
+ *  @date     2017/07/19
+ *  @version  1.0
  */
 
 #pragma once
@@ -17,52 +17,52 @@ namespace Sein
   namespace Direct3D12
   {
     /**
-     *	@brief	バッファクラス
+     *  @brief  バッファクラス
      */
     class Buffer
     {
     public:
       /**
-       *	@brief	コンストラクタ
+       *  @brief  コンストラクタ
        */
       Buffer();
 
       /**
-       *	@brief	デストラクタ
+       *  @brief  デストラクタ
        */
       ~Buffer();
 
       /**
-       *	@brief	リソース(バッファ)を生成する
-       *	@param	device:Direct3D12のデバイス
-       *	@param	properties:リソースのヒーププロパティ
-       *	@param	width:リソースのサイズ
-       *	@param	flag:リソース操作オプションフラグ
+       *  @brief  リソース(バッファ)を生成する
+       *  @param  device:Direct3D12のデバイス
+       *  @param  properties:リソースのヒーププロパティ
+       *  @param  width:リソースのサイズ
+       *  @param  flag:リソース操作オプションフラグ
        */
       void Create(ID3D12Device* const device, const D3D12_HEAP_PROPERTIES& properties, const UINT64 width, const D3D12_RESOURCE_FLAGS flag = D3D12_RESOURCE_FLAG_NONE) noexcept(false);
 
       /**
-       *	@brief	リソース(バッファ)を開放する
+       *  @brief  リソース(バッファ)を開放する
        */
       void Release() noexcept;
 
       /**
-       *	@brief	リソース(バッファ)を取得する
-       *	@return	リソースの参照
+       *  @brief  リソース(バッファ)を取得する
+       *  @return リソースの参照
        */
       ID3D12Resource& Get() const noexcept;
 
     private:
       /**
-       *	@brief	コピーコンストラクタ
-       *	@param	other:コピー元のインスタンス
+       *  @brief  コピーコンストラクタ
+       *  @param  other:コピー元のインスタンス
        */
       Buffer(const Buffer& other) = delete;
 
       /**
-       *	@brief	代入演算子オペレータ
-       *	@param	other:代入元のインスタンス
-       *	@return	代入後のインスタンス
+       *  @brief  代入演算子オペレータ
+       *  @param  other:代入元のインスタンス
+       *  @return 代入後のインスタンス
        */
       Buffer& operator = (const Buffer& other) = delete;
 

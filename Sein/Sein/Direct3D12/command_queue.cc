8c069dbee1c949d181ca952fd60c10d3a21572fa
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2018-03-19 23:05:35 +0900

    コマンドキュー用インターフェイスを作成

---
+++ Sein/Sein/Direct3D12/command_queue.cc
@@ -0,0 +1,90 @@
+/**
+ *  @file     command_queue.cc
+ *  @brief    コマンドキューに関する処理を行うプログラムソース
+ *  @author   kkllPreciel
+ *  @date     2018/03/19
+ *  @version  1.0
+ */
+
+ // include
+#include "command_queue.h"
+
+namespace Sein
+{
+  namespace Direct3D12
+  {
+    namespace
+    {
+      /**
+       *  @brief  コマンドキュー用クラス
+       */
+      class CommandQueue final : public ICommandQueue
+      {
+      public:
+        /**
+         *  @brief  コンストラクタ
+         */
+        CommandQueue() : command_queue_(nullptr, [](ID3D12CommandQueue* p) { p->Release(); })
+        {
+
+        }
+
+        /**
+         *  @brief  デストラクタ
+         */
+        ~CommandQueue() override
+        {
+          Release();
+        }
+
+        /**
+         *  @brief  コマンドキューを設定する
+         *  @param  command_queue:コマンドキュー
+         */
+        void SetCommandQueue(ID3D12CommandQueue* command_queue)
+        {
+          command_queue_.reset(command_queue);
+        }
+
+        /**
+         *  @brief  コマンドキューを取得する
+         *  @return コマンドキュー
+         */
+        const ID3D12CommandQueue& Get() const override
+        {
+          return *command_queue_;
+        }
+
+        /**
+         *  @brief  コマンドキューを開放する
+         */
+        void Release() noexcept override
+        {
+          command_queue_.reset();
+        }
+
+      private:
+        std::unique_ptr<ID3D12CommandQueue, void(*)(ID3D12CommandQueue*)> command_queue_; ///< コマンドキュー
+      };
+    };
+
+    /**
+     *  @brief  コマンドキューを作成する
+     *  @param  device:Direct3D12のデバイス
+     *  @param  command_queue_desc:コマンドキューの設定
+     *  @return コマンドキューへのシェアードポインタ
+     */
+    std::shared_ptr<ICommandQueue> ICommandQueue::Create(ID3D12Device* const device, const D3D12_COMMAND_QUEUE_DESC& command_queue_desc)
+    {
+      ID3D12CommandQueue* queue;
+      if (FAILED(device->CreateCommandQueue(&command_queue_desc, IID_PPV_ARGS(&queue))))
+      {
+        throw std::exception("コマンドキューの生成に失敗しました。");
+      }
+
+      auto instance = std::make_unique<CommandQueue>();
+      instance->SetCommandQueue(queue);
+      return instance;
+    }
+  };
+};
---
+++ Sein/Sein/Direct3D12/command_queue.h
@@ -0,0 +1,68 @@
+/**
+ *  @file     command_queue.h
+ *  @brief    コマンドキューに関するヘッダファイル
+ *  @author   kkllPreciel
+ *  @date     2018/03/19
+ *  @version  1.0
+ */
+
+#pragma once
+
+ // include
+#include <memory>
+#include <d3d12.h>
+
+namespace Sein
+{
+  namespace Direct3D12
+  {
+    /**
+     *  @brief  コマンドキュー用インターフェイス
+     */
+    class ICommandQueue
+    {
+    public:
+      /**
+       *  @brief  コンストラクタ
+       */
+      ICommandQueue() = default;
+
+      /**
+       *  @brief  デストラクタ
+       */
+      virtual ~ICommandQueue() = default;
+
+      /**
+       *  @brief  コマンドキューを取得する
+       *  @return コマンドキュー
+       */
+      virtual const ID3D12CommandQueue& Get() const = 0;
+
+      /**
+       *  @brief  コマンドキューを開放する
+       */
+      virtual void Release() noexcept = 0;
+
+      /**
+       *  @brief  コピーコンストラクタ
+       *  @param  other:コピー元のインスタンス
+       */
+      ICommandQueue(const ICommandQueue& other) = delete;
+
+      /**
+       *  @brief  代入演算子オペレータ
+       *  @param  other:代入元のインスタンス
+       *  @return 代入後のインスタンス
+       */
+      ICommandQueue& operator = (const ICommandQueue& other) = delete;
+
+      /**
+       *  @brief  コマンドキューを作成する
+       *  @param  device:Direct3D12のデバイス
+       *  @param  command_queue_desc:コマンドキューの設定
+       *  @return コマンドキューへのシェアードポインタ
+       */
+      static std::shared_ptr<ICommandQueue> Create(ID3D12Device* const device, const D3D12_COMMAND_QUEUE_DESC& command_queue_desc);
+    };
+  };
+};

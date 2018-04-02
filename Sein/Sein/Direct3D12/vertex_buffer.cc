ca592be177cd9efff3029437af6e633eff678344
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2018-04-02 23:06:51 +0900

    頂点バッファ用インターフェイスを作り直した

--- Sein/Sein/Direct3D12/vertex_buffer.cc
+++ Sein/Sein/Direct3D12/vertex_buffer.cc
@@ -2,42 +2,115 @@
  *  @file     vertex_buffer.cc
  *  @brief    頂点バッファに関する処理を行うプログラムソース
  *  @author   kkllPreciel
- *  @date     2017/05/09
+ *  @date     2018/04/02
  *  @version  1.0
  */
 
  // include
-#include "buffer.h"
 #include "vertex_buffer.h"
+#include "buffer.h"
 
 namespace Sein
 {
   namespace Direct3D12
   {
-    /**
-     *  @brief  コンストラクタ
-     */
-    VertexBuffer::VertexBuffer() : buffer(new Buffer())
+    namespace
     {
+      /**
+       *  @brief  頂点バッファ用クラス
+       */
+      class VertexBuffer final : public IVertexBuffer
+      {
+      public:
+        /**
+         *  @brief  コンストラクタ
+         */
+        VertexBuffer() : buffer_(nullptr), vertex_buffer_view_({})
+        {
 
-    }
+        }
+
+        /**
+         *  @brief  デストラクタ
+         */
+        ~VertexBuffer() override
+        {
+          Release();
+        }
+
+        /**
+         *  @brief  バッファを設定する
+         *  @param  buffer:バッファ
+         */
+        void SetBuffer(Buffer* const buffer)
+        {
+          buffer_.reset(buffer);
+        }
+
+        /**
+         *  @brief  マップする
+         *  @param  stride_in_bytes:1頂点のサイズ
+         *  @param  vertices:頂点配列へのポインタ
+         */
+        void Map(const std::uint32_t stride_in_bytes, const void* const vertices) override
+        {
+          // 頂点バッファ(リソース)へのポインタ
+          unsigned char* pData;
+          if (FAILED(buffer_->Get().Map(
+            0,                                  // サブリソースのインデックス番号
+            nullptr,                            // CPUからアクセスするメモリの範囲(nullptrは全領域にアクセスする)
+            reinterpret_cast<void**>(&pData)    // リソースデータへのポインタ
+          )))
+          {
+            throw std::exception("頂点バッファ用リソースへのポインタの取得に失敗しました。");
+          }
+
+          // 頂点バッファ(リソース)へ頂点データをコピー
+          std::memcpy(pData, vertices, buffer_->Get().GetDesc().Width);
+
+          // 頂点バッファ(リソース)へのポインタを無効にする
+          buffer_->Get().Unmap(
+            0,      // サブリソースインデックス番号
+            nullptr // マップ解除するメモリの範囲、CPUが変更した可能性のある領域(nullptrは全領域)
+          );
+
+          // 頂点バッファのビューを初期化する
+          vertex_buffer_view_.BufferLocation = buffer_->Get().GetGPUVirtualAddress(); // バッファのアドレス
+          vertex_buffer_view_.SizeInBytes = buffer_->Get().GetDesc().Width;           // バッファ(全頂点合計)のサイズ(バイト単位)
+          vertex_buffer_view_.StrideInBytes = stride_in_bytes;                        // 1頂点のサイズ(バイト単位)
+        }
+
+        /**
+         *  @brief  頂点バッファビューを取得する
+         *  @return 頂点バッファビュー
+         */
+        const D3D12_VERTEX_BUFFER_VIEW& GetView() override
+        {
+          return vertex_buffer_view_;
+        }
+        
+        /**
+         *  @brief  頂点バッファを開放する
+         */
+        void Release() noexcept override
+        {
+          vertex_buffer_view_ = {};
+          buffer_.reset();
+        }
+
+      private:
+        std::unique_ptr<Buffer> buffer_;              ///< バッファ(リソース)
+        D3D12_VERTEX_BUFFER_VIEW vertex_buffer_view_; ///< 頂点バッファビュー
+      };
+    };
 
     /**
-     *  @brief  デストラクタ
-     */
-    VertexBuffer::~VertexBuffer()
-    {
-      buffer.reset(nullptr);
-    }
-
-    /**
-     *  @brief  頂点バッファの生成を行う
+     *  @brief  頂点バッファを作成する
      *  @param  device:Direct3D12のデバイス
-     *  @param  size:頂点バッファのサイズ(全頂点合計サイズ)
-     *  @param  stride:1頂点のサイズ
-     *  @param  vertices:頂点データ
+     *  @param  size_in_bytes:頂点バッファのサイズ(頂点サイズ * 頂点数)
+     *  @return 頂点バッファへのユニークポインタ
      */
-    void VertexBuffer::Create(ID3D12Device* const device, const unsigned int size, const unsigned short stride, const void* const vertices)
+    std::unique_ptr<IVertexBuffer> IVertexBuffer::Create(ID3D12Device* const device, const std::uint32_t size_in_bytes)
     {
       // ヒープの設定
       D3D12_HEAP_PROPERTIES properties;
@@ -47,47 +120,20 @@ namespace Sein
       properties.CreationNodeMask = 1;                              // 恐らくヒープが生成されるアダプター(GPU)の番号
       properties.VisibleNodeMask = 1;                               // 恐らくヒープが表示されるアダプター(GPU)の番号
 
+      auto buffer = new Buffer();
+      auto vertex_buffer = std::make_unique<VertexBuffer>();
+      vertex_buffer->SetBuffer(buffer);
+
       try
       {
-        buffer->Create(device, properties, size, D3D12_RESOURCE_FLAG_NONE);
+        buffer->Create(device, properties, size_in_bytes, D3D12_RESOURCE_FLAG_NONE);
       }
       catch (const char*)
       {
-        throw "頂点バッファ用リソースの作成に失敗しました。";
+        throw std::exception("頂点バッファ用リソースの作成に失敗しました。");
       }
 
-      // 頂点バッファ(リソース)へのポインタ
-      unsigned char* pData;
-      if (FAILED(buffer->Get().Map(
-        0,                                  // サブリソースのインデックス番号
-        nullptr,                            // CPUからアクセスするメモリの範囲(nullptrは全領域にアクセスする)
-        reinterpret_cast<void**>(&pData)))) // リソースデータへのポインタ
-      {
-        throw "頂点バッファ用リソースへのポインタの取得に失敗しました。";
-      }
-
-      // 頂点バッファ(リソース)へ頂点データをコピー
-      std::memcpy(pData, vertices, size);
-
-      // 頂点バッファ(リソース)へのポインタを無効にする
-      buffer->Get().Unmap(
-        0,      // サブリソースインデックス番号
-        nullptr // マップ解除するメモリの範囲、CPUが変更した可能性のある領域(nullptrは全領域)
-      );
-
-      // 頂点バッファのビューを初期化する
-      view.BufferLocation = buffer->Get().GetGPUVirtualAddress(); // バッファのアドレス
-      view.SizeInBytes = size;                                    // バッファ(全頂点合計)のサイズ(バイト単位)
-      view.StrideInBytes = stride;                                // 1頂点のサイズ(バイト単位)
-    }
-
-    /**
-     *  @brief  ビューを取得する
-     *  @return ビューへの参照
-     */
-    const D3D12_VERTEX_BUFFER_VIEW& VertexBuffer::GetView() const
-    {
-      return view;
+      return vertex_buffer;
     }
   };
 };
--- Sein/Sein/Direct3D12/vertex_buffer.h
+++ Sein/Sein/Direct3D12/vertex_buffer.h
@@ -2,7 +2,7 @@
  *  @file     vertex_buffer.h
  *  @brief    頂点バッファに関するヘッダファイル
  *  @author   kkllPreciel
- *  @date     2017/05/08
+ *  @date     2018/04/02
  *  @version  1.0
  */
 
@@ -16,56 +16,60 @@ namespace Sein
 {
   namespace Direct3D12
   {
-    class Buffer;
-
     /**
-     *  @brief  頂点バッファクラス
+     *  @brief  頂点バッファ用インターフェイス
      */
-    class VertexBuffer
+    class IVertexBuffer
     {
     public:
       /**
        *  @brief  コンストラクタ
        */
-      VertexBuffer();
+      IVertexBuffer() = default;
 
       /**
        *  @brief  デストラクタ
        */
-      ~VertexBuffer();
+      virtual ~IVertexBuffer() = default;
 
       /**
-       *  @brief  頂点バッファの生成を行う
-       *  @param  device:Direct3D12のデバイス
-       *  @param  size:頂点バッファのサイズ(全頂点合計サイズ)
-       *  @param  stride:1頂点のサイズ
-       *  @param  vertices:頂点データ
+       *  @brief  マップする
+       *  @param  stride_in_bytes:1頂点のサイズ
+       *  @param  vertices:頂点配列へのポインタ
        */
-      void Create(ID3D12Device* const device, const unsigned int size, const unsigned short stride, const void* const vertices);
+      virtual void Map(const std::uint32_t stride_in_bytes, const void* const vertices) = 0;
 
       /**
-       *  @brief  ビューを取得する
-       *  @return ビューへの参照
+       *  @brief  頂点バッファビューを取得する
+       *  @return 頂点バッファビュー
        */
-      const D3D12_VERTEX_BUFFER_VIEW& GetView() const;
+      virtual const D3D12_VERTEX_BUFFER_VIEW& GetView() = 0;
 
-    private:
+      /**
+       *  @brief  頂点バッファを開放する
+       */
+      virtual void Release() noexcept = 0;
+
       /**
        *  @brief  コピーコンストラクタ
        *  @param  other:コピー元のインスタンス
        */
-      VertexBuffer(const VertexBuffer& other) = delete;
+      IVertexBuffer(const IVertexBuffer& other) = delete;
 
       /**
        *  @brief  代入演算子オペレータ
        *  @param  other:代入元のインスタンス
        *  @return 代入後のインスタンス
        */
-      VertexBuffer& operator = (const VertexBuffer& other) = delete;
+      IVertexBuffer& operator = (const IVertexBuffer& other) = delete;
 
-    private:
-      std::unique_ptr<Buffer>    buffer;  ///< バッファ(リソース)
-      D3D12_VERTEX_BUFFER_VIEW  view;     ///< 頂点バッファのビュー
+      /**
+       *  @brief  頂点バッファを作成する
+       *  @param  device:Direct3D12のデバイス
+       *  @param  size_in_bytes:頂点バッファのサイズ(頂点サイズ * 頂点数)
+       *  @return 頂点バッファへのユニークポインタ
+       */
+      static std::unique_ptr<IVertexBuffer> Create(ID3D12Device* const device, const std::uint32_t size_in_bytes);
     };
   };
 };

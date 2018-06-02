d78c224bdc19f776496a23f6875b8b2047c16601
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2018-06-02 22:53:11 +0900

    Direct3D11デバイス作成処理を追加

--- Sein/Sein/Direct3D11/direct3d11_device.cc
+++ Sein/Sein/Direct3D11/direct3d11_device.cc
@@ -9,6 +9,8 @@
  // include
 #include "direct3d11_device.h"
 #include <cassert>
+#include <array>
+#include <functional>
 
 namespace Sein
 {
@@ -60,14 +62,82 @@ namespace Sein
         void Create(const HWND handle, const std::uint32_t width, const std::uint32_t height)
         {
           assert(device_ == nullptr);
+
+          UINT createDeviceFlags = 0;
+#ifdef _DEBUG
+          createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+          // ドライバー種別
+          std::array<D3D_DRIVER_TYPE, 3> driver_types = {
+            D3D_DRIVER_TYPE_HARDWARE,
+            D3D_DRIVER_TYPE_WARP,
+            D3D_DRIVER_TYPE_REFERENCE,
+          };
+
+          // 機能レベル
+          std::array<D3D_FEATURE_LEVEL, 2> feature_levels = {
+            D3D_FEATURE_LEVEL_11_1,
+            D3D_FEATURE_LEVEL_11_0
+          };
+
+          // スワップチェインの設定
+          DXGI_SWAP_CHAIN_DESC swap_chain_desc = {0};
+          swap_chain_desc.BufferCount = 1;
+          swap_chain_desc.BufferDesc.Width = width;
+          swap_chain_desc.BufferDesc.Height = height;
+          swap_chain_desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+          swap_chain_desc.BufferDesc.RefreshRate.Numerator = 60;
+          swap_chain_desc.BufferDesc.RefreshRate.Denominator = 1;
+          swap_chain_desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+          swap_chain_desc.OutputWindow = handle;
+          swap_chain_desc.SampleDesc.Count = 1;
+          swap_chain_desc.SampleDesc.Quality = 0;
+          swap_chain_desc.Windowed = TRUE;
+
+          for (auto driver_type : driver_types)
+          {
+            ID3D11Device* device;
+            ID3D11DeviceContext* context;
+            IDXGISwapChain* swap_chain;
+
+            // デバイスとスワップチェインを作成する
+            // デバイスのみで作成することも可能
+            // その場合、スワップチェインは後で作成する
+            if (SUCCEEDED(D3D11CreateDeviceAndSwapChain(
+              nullptr,
+              driver_type,
+              nullptr,
+              createDeviceFlags,
+              feature_levels.data(),
+              static_cast<UINT>(feature_levels.size()),
+              D3D11_SDK_VERSION,
+              &swap_chain_desc,
+              &swap_chain,
+              &device,
+              &feature_level_,
+              &context)))
+            {
+              device_.reset(device);
+              context_.reset(context);
+              swap_chain_.reset(swap_chain);
+              driver_type_ = driver_type;
+
+              break;
+            }
+          }
+
+          if (!device_ || !context_ || !swap_chain_)
+          {
+            throw std::exception("デバイスの生成に失敗しました。");
+          }
         }
 
       private:
-        std::shared_ptr<ID3D11Device> device_;          ///< デバイス
-        std::shared_ptr<ID3D11DeviceContext> context_;  ///< コンテキスト
-        std::shared_ptr<IDXGISwapChain> swap_chain_;    ///< スワップチェーン
-        D3D_DRIVER_TYPE driver_type_;                   ///< ドライバー種別
-        D3D_FEATURE_LEVEL feature_level_;               ///< 機能レベル
+        std::unique_ptr<ID3D11Device, std::function<void(ID3D11Device*)>> device_;                ///< デバイス
+        std::unique_ptr<ID3D11DeviceContext, std::function<void(ID3D11DeviceContext*)>> context_; ///< コンテキスト
+        std::unique_ptr<IDXGISwapChain, std::function<void(IDXGISwapChain*)>> swap_chain_;        ///< スワップチェーン
+        D3D_DRIVER_TYPE driver_type_;                                                             ///< ドライバー種別
+        D3D_FEATURE_LEVEL feature_level_;                                                         ///< 機能レベル
       };
     };
 

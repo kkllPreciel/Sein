ec91e48a1d2f31e03abdb0bd18686068efaf4818
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2018-06-07 22:47:37 +0900

    Direct3D11のデバイスコンテキストインターフェースを追加

---
+++ Sein/Sein/Direct3D11/direct3d11_device_context.cc
@@ -0,0 +1,90 @@
+/**
+ *  @file     direct3d11_device_context.cc
+ *  @brief    Direct3D11のデバイスコンテキストに関する処理を行うプログラムソース
+ *  @author   kkllPreciel
+ *  @date     2018/06/07
+ *  @version  1.0
+ */
+
+ // include
+#include "direct3d11_device_context.h"
+#include <functional>
+
+namespace Sein
+{
+  namespace Direct3D11
+  {
+    namespace
+    {
+      class DeviceContext final : public IDeviceContext
+      {
+      public:
+        /**
+         *  @brief  コンストラクタ
+         */
+        DeviceContext() : context_(nullptr, [](ID3D11DeviceContext* p) { p->Release(); })
+        {
+
+        }
+
+        /**
+         *  @brief  デストラクタ
+         */
+        ~DeviceContext() override
+        {
+          Destroy();
+        }
+
+        /**
+         *  @brief  作成する
+         *  @param  device:Direct3D11のデバイス
+         *  @param  context_type:デバイスコンテキストの種別
+         *  @param  type:インデックスの型
+         */
+        void Create(ID3D11Device* const device, IDeviceContext::Type context_type)
+        {
+          ID3D11DeviceContext* context;
+          if (context_type == Type::DEFERRED)
+          {
+            device->CreateDeferredContext(0, &context);
+          }
+          else
+          {
+            device->GetImmediateContext(&context);
+          }
+
+          context_.reset(context);
+          type_ = context_type;
+        }
+        
+        /**
+         *  @brief  終了処理を行う
+         */
+        void Destroy() override
+        {
+          context_->Release();
+        }
+
+      private:
+        std::unique_ptr<ID3D11DeviceContext, std::function<void(ID3D11DeviceContext*)>> context_; ///< デバイスコンテキスト
+        Type type_;                                                                               ///< デバイスコンテキストの種別
+
+      };
+    };
+
+    /**
+     *  @brief  デバイスコンテキストを作成する
+     *  @param  device:Direct3D11のデバイス
+     *  @param  context_type:デバイスコンテキストの種別
+     *  @return デバイスコンテキストへのシェアードポインタ
+     */
+    std::shared_ptr<IDeviceContext> IDeviceContext::Create(ID3D11Device* const device, IDeviceContext::Type context_type)
+    {
+      auto device_context = std::make_shared<DeviceContext>();
+
+      device_context->Create(device, context_type);
+
+      return device_context;
+    }
+  };
+};
---
+++ Sein/Sein/Direct3D11/direct3d11_device_context.h
@@ -0,0 +1,70 @@
+/**
+ *  @file     direct3d11_device_context.h
+ *  @brief    Direct3D11のデバイスコンテキストに関するヘッダファイル
+ *  @author   kkllPreciel
+ *  @date     2018/06/06
+ *  @version  1.0
+ */
+
+#pragma once
+
+// include
+#include <memory>
+#include <d3d11.h>
+
+namespace Sein
+{
+  namespace Direct3D11
+  {
+    /**
+     *  @brief  Direct3D11のデバイスコンテキスト用インターフェース
+     */
+    class IDeviceContext
+    {
+    public:
+      /**
+       *  @brief  コンテキストの種別
+       */
+      enum class Type : std::uint32_t {
+        IMMEDIATE,  ///< 即時
+        DEFERRED,   ///< 遅延
+      };
+
+      /**
+       *  @brief  コンストラクタ
+       */
+      IDeviceContext() = default;
+      
+      /**
+       *  @brief  デストラクタ
+       */
+      virtual ~IDeviceContext() = default;
+      
+      /**
+       *  @brief  コピーコンストラクタ
+       *  @param  other:コピー元のインスタンス
+       */
+      IDeviceContext(const IDeviceContext& other) = delete;
+      
+      /**
+       *  @brief  代入演算子オペレータ
+       *  @param  other:代入元のインスタンス
+       *  @return 代入後のインスタンス
+       */
+      IDeviceContext& operator = (const IDeviceContext& other) = delete;
+      
+      /**
+       *  @brief  終了処理を行う
+       */
+      virtual void Destroy() = 0;
+
+      /**
+       *  @brief  デバイスコンテキストを作成する
+       *  @param  device:Direct3D11のデバイス
+       *  @param  context_type:デバイスコンテキストの種別
+       *  @return デバイスコンテキストへのシェアードポインタ
+       */
+      static std::shared_ptr<IDeviceContext> Create(ID3D11Device* const device, IDeviceContext::Type context_type);
+    };
+  };
+};
